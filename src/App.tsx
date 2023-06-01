32566c4f6a42e6fb82c7750f8050b1e55be6f168
Lucas Araujo <lucas984618740@gmail.com>
2023-06-01 00:12:21 -0300

    feat:initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import { Routes, Route} from "react-router-dom";
+function App() {
+  return (
+    <>
+    
+    {/* <Routes>
+		    <Route path="/" element={}/> 
+		    <Route path="*" element={}/> 
+    </Routes> */}
+    
+    </>
+  )
+}
+
+export default App
