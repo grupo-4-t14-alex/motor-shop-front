175350fef6aaa9933a2248cb3a58ec6586e181ca
Pedro Lisboa <lisboa.ti.mkt@gmail.com>
2023-06-07 13:06:58 -0300

    feat: structure navBar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,11 +1,39 @@
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+} from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 
 
 export const NavBarComponent = () => {
+
+  const navigate = useNavigate()
+
   return (
     <header>
-        <h1>Hello</h1>
+        <h1>Motors shop</h1>
         <div>
-            
+        <Menu>
+          {({ isOpen }) => (
+            <>
+              <MenuButton isActive={isOpen} as={Button} rightIcon={isOpen ? <CloseIcon/> : <HamburgerIcon/> }>
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Carros</MenuItem>
+                <MenuItem>Motos</MenuItem>
+                <MenuItem>Leilão</MenuItem>
+                <div>
+
+                </div>
+                <MenuItem onClick={() => navigate("/")}>Fazer Login</MenuItem>
+              </MenuList>
+            </>
+          )}
+        </Menu>
         </div>
     </header>
   )
