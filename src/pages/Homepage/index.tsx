bb4e42c6969b11cabfbbad334f0acc62f4c32468
ViniciusOgawa <vini_ogawa@hotmail.com>
2023-06-09 15:10:47 -0300

    fix: fixed style home page

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -2,24 +2,23 @@ import { Container, Image } from "@chakra-ui/react";
 import { NavBarComponent } from "../../components/NavBar";
 import { Footer } from "../../components/Footer";
 import { Filter } from "../../components/Filter";
+import imgHomePage from "../../assets/img/imgHomePage.png";
 
 export const Homepage = () => {
   return (
     <>
       <NavBarComponent />
-      <Container minH="100vh" height="100%" maxW="1600px" pb="60px">
+      <Container
+        minH="100vh"
+        height="100%"
+        maxW="1600px"
+        paddingBottom={"200px"}
+        marginBottom={"500px"}
+        marginTop={"100px"}
+      >
         <Image src={imgHomePage} />
         <Filter />
-        <Footer />
       </Container>
-      <Box bg="grey.1" w="100%" p={4} color="white">
-        Box Test
-      </Box>
-      <Button variant="solid" bg="brand.2" color="whiteFixed">
-        Teste
-      </Button>
-      <Filter />
-      <CardCarOwner />
       <Footer />
     </>
   );
