3f34dc96ff1acd77afbe93ae9f8702c16bd4bfe4
Pedro Lisboa <lisboa.ti.mkt@gmail.com>
2023-06-09 03:57:52 -0300

    fix: button burger menu

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -7,14 +7,24 @@ import {
   Button,
   Flex,
   Img,
+  useMediaQuery,
 } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
 import Logo from "../../assets/img/logoHeader.png"
+import { useEffect, useState } from 'react'
 
 
 export const NavBarComponent = () => {
 
   const navigate = useNavigate()
+  
+  const [web, setWeb] = useState(true)
+
+  const [isMobileScreen] = useMediaQuery("(max-width: 768px)");
+
+  useEffect(() => {
+    setWeb(!isMobileScreen);
+  }, [isMobileScreen]);
 
   return (
     <Flex as={"header"}
@@ -25,10 +35,16 @@ export const NavBarComponent = () => {
       paddingInline={{base: "60px", md: "60px"}}
       >
         <Img src={Logo} w={"153px"} h={"27px"} />
+        {
+          web ? (
+
           <Flex display={"flex"}>
             <Button onClick={() => navigate("/login")} bg={"none"} color={"grey.2"}>Fazer Login</Button>
             <Button onClick={() => navigate("/register")} bg={"none"} border={"1px"} borderColor={"grey.0"}>Cadastrar</Button>
-            {/* <Menu>
+          </Flex>
+          ) : (
+
+            <Menu>
               {({ isOpen }) => (
                 <>
                   <MenuButton isActive={isOpen} as={Button} rightIcon={isOpen ? <CloseIcon/> : <HamburgerIcon/> }>
@@ -44,8 +60,9 @@ export const NavBarComponent = () => {
                   </MenuList>
                 </>
               )}
-            </Menu> */}
-          </Flex>
+            </Menu>
+          )
+        }
     </Flex>
   )
 }
