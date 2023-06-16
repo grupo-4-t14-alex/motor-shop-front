9359f2622869dbd9a3264585b22bc87e93ac251e
Sarah Venske Costa <sarahvenske@gmail.com>
2023-06-16 16:44:07 -0300

    feat: login page style

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, FormControl, FormLabel, Heading, Input, Link } from "@chakra-ui/react"
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Heading, Input, Link } from "@chakra-ui/react"
 import { NavBarComponent } from "../../components/NavBar";
 import { Footer } from "../../components/Footer";
 import { useForm } from "react-hook-form"
@@ -13,62 +13,84 @@ export const Login = () => {
     const { signIn } = useAuth()
     const navigate = useNavigate();
 
-    const { register, handleSubmit } = useForm<LoginData>({
+    const { register, handleSubmit, formState: { errors } } = useForm<LoginData>({
         resolver: zodResolver(loginSchema)
     })
 
 
     return (
     <Box 
-        bg={"grey.9"} 
-        w={"100%"} 
-        h={"100vh"} 
         display={"flex"}
         justifyContent={"center"} 
         alignItems={"center"}
+        bg={"grey.9"} 
+        w={"100%"} 
+        h={"100vh"} 
         >
         <NavBarComponent/>
+
         <Box
+            as="form" onSubmit={handleSubmit(signIn)}
             display={"flex"}
             flexDirection={"column"}
-            alignContent={"center"}
             justifyContent={"center"}
-            alignItems={"center"}
-            minW={"45%"}
+            alignItems={"stretch"}
+            h={"50%"}
+            maxW={"500px"}
+            w={"60%"}
             bg={"whiteFixed"}
-            borderRadius={"4px"}
-            padding={"5%"}
+            borderRadius={"10px"}
+            padding={"7%"}
         >
-            <form onSubmit={handleSubmit(signIn)} >
-                <FormControl
-                    gap={"10px"} 
-                    display={"flex"} 
-                    flexDirection={"column"}
-                >
-                    <Heading 
-                        fontSize={"heading.3"} 
-                        textAlign={"left"} 
-                        marginBottom={"15px"}>Login</Heading>
+            <Heading 
+                fontSize={"heading.4"} 
+                marginBottom={"40px"}>Login</Heading>
 
-                    <FormLabel fontSize={"inputLabel"}>Email</FormLabel>
-                    <Input {...register("email")}
-                        isRequired type="email" id="email" placeholder="Digitar email" marginBottom={"15px"} />
+            <FormControl>
+                <FormLabel 
+                    fontSize={"heading.7"}>Email</FormLabel>
+                <Input 
+                    {...register("email")}
+                    isRequired type="email" 
+                    id="email" 
+                    placeholder="Digitar email" 
+                    marginBottom={"30px"} />
+                <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+            </FormControl>
+
+            <FormControl>
+                <FormLabel 
+                fontSize={"heading.7"}>Password</FormLabel>
+                <Input 
+                    {...register("password")} 
+                    isRequired 
+                    type="password" 
+                    id="password" 
+                    placeholder="Digitar senha" 
+                    marginBottom={"5px"}/>
+                <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+            </FormControl>
+
+            <Link 
+                marginBottom={"40px"} 
+                textAlign={"right"}
+                fontSize={"heading.7"} >Esqueci minha senha</Link>
+
+            <Button 
+                type="submit" 
+                marginBottom={"40px"} 
+                variant={"brand1"}>Entrar</Button>
+
+            <Heading 
+                fontSize={"heading.7"} 
+                textAlign={"center"}
+                marginBottom={"30px"}>Ainda não possui conta?</Heading>
+
+            <Button 
+                onClick={() => navigate("/register")} 
+                marginBottom={"30px"} 
+                variant={"outline2"}>Cadastrar</Button>
                 
-
-                    <FormLabel>Password</FormLabel>
-                    <Input {...register("password")} 
-                        isRequired type="password" id="password" placeholder="Digitar senha" marginBottom={"15px"}  />
-                
-
-                    <Link marginBottom={"30px"}>Esqueci minha senha</Link>
-
-                    <Button type="submit" onClick={() => console.log("cheguei")} marginBottom={"20px"}>Entrar</Button>
-
-                    <Heading fontSize={"heading.5"} marginBottom={"20px"}>Ainda não possui conta?</Heading>
-
-                    <Button onClick={() => navigate("/register")}>Cadastrar</Button>
-                </FormControl>
-            </form>
         </Box>
         <Footer/>
     </Box>
