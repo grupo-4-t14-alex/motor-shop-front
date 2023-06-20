0c84df30e61320fe3544452015d23e152e15c742
Pedro Lisboa <102534718+Lisboaseyth@users.noreply.github.com>
2023-06-20 14:22:15 -0300

    feat/registerPage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import { Homepage } from "../pages/homepage";
+import { Homepage } from "../pages/Homepage";
 import { Product } from "../pages/Product";
 import { RegisterPage } from "../pages/Register";
 import { ProfileViewAdmin } from "../pages/ProfileViewAdmin";
-import { TestPage } from "../pages/testPage";
+// import { TestPage } from "../pages/testPage";
 import { Login } from "../pages/Login";
 import { ProtectedRoutes } from "./ProtectedRoutes";
 
@@ -14,8 +14,7 @@ const RoutesPages = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
-        
-        {/* <Route path="/login" element={} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route element={<ProtectedRoutes />}>
           <Route path="/product" element={<Product />} />
           <Route path="/profileViewAdmin" element={<ProfileViewAdmin />} />
